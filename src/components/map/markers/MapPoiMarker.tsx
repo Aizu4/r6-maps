9b96fa753d009d1fc084838df569134b566df3c1
aizu <s@>
2026-04-26 02:07:48 +0200

    refactor waypoints

--- src/components/map/markers/MapPoiMarker.tsx
+++ src/components/map/markers/MapPoiMarker.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Tooltip} from 'antd';
+import {Avatar, Tooltip} from 'antd';
 import type {Poi, PoiType} from '../../../types';
 import {POI_STYLES} from '../../../types';
 
@@ -8,15 +8,18 @@ interface MapPoiMarkerProps {
   waypoint: Poi;
 }
 
-const className = 'absolute -translate-x-1/2 -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center text-white text-xs font-bold cursor-pointer pointer-events-auto';
 
 export default function MapPoiMarker({type, waypoint}: MapPoiMarkerProps) {
   const {color, label} = POI_STYLES[type];
-  const markerStyle: React.CSSProperties = {left: waypoint.position.x, top: waypoint.position.y, backgroundColor: color};
+  const markerStyle: React.CSSProperties = {
+    left: waypoint.position.x,
+    top: waypoint.position.y,
+    backgroundColor: color
+  };
 
   return (
-    <Tooltip title={waypoint.note}>
-      <div className={className} style={markerStyle}>{label}</div>
-    </Tooltip>
+      <Tooltip title={waypoint.note}>
+        <Avatar className={'pointer-events-auto'} style={markerStyle}>{label}</Avatar>
+      </Tooltip>
   );
 }
--- src/components/map/markers/MapSpawnMarker.tsx
+++ src/components/map/markers/MapSpawnMarker.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import {Tooltip} from 'antd';
+import {Avatar, Tooltip} from 'antd';
 import type {SpawnLocation} from '../../../types';
 
 interface MapSpawnMarkerProps {
   spawn: SpawnLocation;
 }
 
-const className = 'absolute -translate-x-1/2 -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center text-white text-xs font-bold cursor-pointer pointer-events-auto';
 const color = '#22c55e';
 
 export default function MapSpawnMarker({spawn}: MapSpawnMarkerProps) {
   const markerStyle: React.CSSProperties = {left: spawn.position.x, top: spawn.position.y, backgroundColor: color};
 
   return (
-    <Tooltip title={spawn.name}>
-      <div className={className} style={markerStyle}>S</div>
-    </Tooltip>
+      <Tooltip title={spawn.name}>
+        <Avatar className={'pointer-events-auto'} style={markerStyle}>S</Avatar>
+      </Tooltip>
   );
 }
