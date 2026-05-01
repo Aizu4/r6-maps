70403463ea1f26c07696a510a08d7150b0e01421
Aizu <placeholder@example.com>
2026-05-01 14:15:34 +0200

    types refactor [wip]

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,53 +1,61 @@
-export interface Position {
-  x: number;
-  y: number;
-  floors?: number[];
+export interface Point {
+    x: number;
+    y: number;
 }
 
-export interface Waypoint {
-  position: Position;
-  note?: string;
+export interface Position extends Point {
+    floors: number[];
 }
 
-export interface NamedWaypoint extends Waypoint {
-  name: string;
+export interface NamedId {
+    id: number;
+    name: string;
 }
 
-export interface Room extends NamedWaypoint {
-  poly: Position[]
+export type NamedPoint = NamedId & Point;
+export type NamedPosition = NamedId & Position;
+
+export interface Room extends NamedPoint {
+    poly: Point[];
+}
+
+export interface BombLocation extends NamedId {
+    position_a: NamedPosition;
+    position_b: NamedPosition;
 }
 
 // Map JSON layout
 
 export interface MapData {
-  display_name: string;
-  floors: { id: number; name: string }[];
-  spawn_locations: { id: number, name: string, position: Position }[];
-  bomb_locations: { id: number, name: string, position_a: Position, position_b: Position }[];
-  waypoints: {
-    cameras?: Waypoint[];
-    fire_extinguishers?: Waypoint[];
-    gas_pipes?: Waypoint[];
-    hatches?: Waypoint[];
-    stairs?: Waypoint[];
-    ladders?: Waypoint[];
-  };
-  rooms: Room[];
-  metadata: MapMetadata;
+    display_name: string;
+    floors: NamedId[];
+    spawn_locations: NamedPoint[];
+    bomb_locations: BombLocation[];
+    waypoints: {
+        cameras?: Position[];
+        fire_extinguishers?: Position[];
+        gas_pipes?: Position[];
+        hatches?: Position[];
+        stairs?: Position[];
+        ladders?: Position[];
+        drone_vents?: Position[];
+    };
+    rooms: Room[];
+    metadata: MapMetadata;
 }
 
 export interface MapMetadata {
-  slug: string;
-  blueprint_url?: string;
-  blueprint_path: string;
-  background_color?: string;
-  waypoint_size: number;
-  highlight_color?: string;
+    slug: string;
+    blueprint_url?: string;
+    blueprint_path: string;
+    background_color?: string;
+    waypoint_size: number;
+    highlight_color?: string;
 }
 
 export interface Option<T> {
-  label: string;
-  value: T;
-  children?: Option<T>[];
+    label: string;
+    value: T;
+    children?: Option<T>[];
 }
 
